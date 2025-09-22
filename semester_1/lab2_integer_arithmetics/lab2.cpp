6bb14fbb7fa57b6b612e240865e114baac0219e0
rachel.i.jonathan.pobedili@gmail.com <rachel.i.jonathan.pobedili@gmail.com>
2025-09-22 21:41:01 +0300

    add lab2

---
+++ semester_1/lab2_integer_arithmetics/lab2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+int main() 
+{
+	int n, paln = 0, sqrn = 0, palSqrn = 0, l = 0, num = 0;
+	std::cout << "enter natural number: ";
+	std::cin >> n;
+	std::cout << "doube palindrome numbers: ";
+	for (int i = 1; (i <= n);)
+	{	l = i;
+	while (l != 0)
+		{	num = l % 10;
+			paln = 10 * paln + num;
+			l = l / 10;
+		} 
+	l = i * i;
+	palSqrn = i * i;
+	while (l != 0)
+		{	num = l % 10;
+			sqrn = 10 * sqrn + num;		
+			l = l / 10;
+		} 
+
+	if (paln == i && sqrn == palSqrn) std::cout << paln << " ";
+		i++;
+		paln = 0;
+		sqrn = 0;
+	}
+	return 0;
+}
