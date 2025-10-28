41d5dfb9d72688a1cd32c150b5eb70c30015094c
rachel.i.jonathan.pobedili@gmail.com <rachel.i.jonathan.pobedili@gmail.com>
2025-10-28 09:09:20 +0300

    lab 3 add

--- semester_1/lab3_pointers_and_arrays/lab3(20).cpp
+++ semester_1/lab3_pointers_and_arrays/lab3(20).cpp
@@ -1,126 +1,12 @@
-<<<<<<< HEAD
 #include <iostream>
+
+int Sum(int* arr, int n);
+void Max(int* arr, int n);
+void DeleteElements(int* arr, int n, int left_board, int right_board);
 
 int main()
 {
-    int arr[1000], sumaft = 0, sumbef = 0, upper_board, lower_board, change, left_board = 0, right_board, compare = 11110, max;
-        int how, j = 0, i,n;
-
-    std::cout << "Enter the amount of elemens: ";
-    std::cin >> n;
-
-    if ((n <= 0) || (n > 1000)) return 0;
-    std::cout << "enter 0, if the elements of array are entered randomly" << std::endl << "Enter 1 if the elements of array are entered by hand";
-
-    std::cin >> how;
-    if (how == 1)
-    {
-        for ((i = 0); (i < n); i++)
-        {
-            std::cout << "enter the element number" << (i + 1) << " ";
-            std::cin >> arr[i];
-        }
-    }
-    else if (how == 0)
-    {
-        std::cout << "Enter the lower board of array";
-        std::cin >> lower_board;
-        std::cout << "Enter the upper board of array";
-        std::cin >> upper_board;
-        if (lower_board > upper_board)
-        {
-            change = lower_board;
-            lower_board = upper_board;
-            upper_board = change;
-        }
-        for ((i = 0); (i < n); i++)
-        {
-            arr[i] = lower_board + rand() % (upper_board - lower_board);
-            std::cout << arr[i] << " ";
-        }
-    }
-    else
-    {
-        std::cout << "ONLY 1 OR 0";
-        return 0;
-    }
-
-    //task 20.1
-    int num;
-    for ((i = 0); (i < n); i++)
-    {
-        sumaft += arr[i];
-    }
-    sumaft -= arr[0];
-
-    for ((i = 1); (i < n); i++)
-    {
-        sumbef += arr[i - 1];
-        sumaft -= arr[i];
-
-        j = sumbef - sumaft;
-        if (abs(j) <= compare)
-        {
-            compare = abs(j);
-           num = i + 1;
-        }
-    }
-    std::cout << std::endl <<"the number of element that the sum of the elements before it differs the least from the sum of the elements after it is " << num << std::endl;
-
-    //task 20.2
-     max = arr[0];
-    for (i = 1; (i <= n); i++)
-        if (max < arr[i])
-        {
-            max = arr[i];
-            j = i;
-        }
-    std::cout <<"the biggest element is "<< max << std::endl << " the number of the biggest element is " << (j + 1) << std::endl;
-
-
-    //extra
-
-    j = 0;
-    std::cout << "enter lower board of  section";
-    std::cin >> left_board;
-    std::cout << "enter upper board of  section";
-    std::cin >> right_board;
-    if (left_board > right_board)
-    {
-        change = left_board;
-        left_board = right_board;
-        right_board = change;
-    }
-
-    int k = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (!(left_board <= arr[i] && arr[i] <= right_board))
-        {
-            if (i != k) arr[k] = arr[i];
-            ++k;
-        }
-    }
-
-    for (int i = k; i < n; i++) 
-        arr[i] = 0;
-
-    for (int i = 0; i < n; i++) 
-        std::cout << arr[i] << ' ';
-
-
-
-
-
-
-    return 0;
-=======
-#include <iostream>
-
-int main()
-{
-    int arr[1000], sumaft = 0, sumbef = 0, j = 0, i, upper_board, lower_board, n, change, left_board=0, right_board, compare = 11110;;
+    int arr[1000], sumaft = 0, sumbef = 0, j = 0, i, upper_board, lower_board, n, change, left_board = 0, right_board, compare = 11110;;
     bool how;
 
     std::cout << "Enter the amount of elemens: ";
@@ -163,12 +49,30 @@ int main()
     }
 
     //task 20.1
-    for ((i = 0); (i < n); i++)
+
+    std::cout << std::endl << Sum(arr, n) << std::endl;
+    //task 20.2
+
+    Max(arr, n);
+
+    //extra
+
+    DeleteElements(arr, n, left_board, right_board);
+
+
+
+
+    return 0;
+}
+
+
+int Sum(int* arr, int n) {
+    int sumbef = 0, sumaft = 0, i = 0, j, num = 0;
+    for ((i = 1); (i < n); i++)
     {
         sumaft += arr[i];
     }
-
-    sumaft -= arr[0];
+    int compare = sumaft;
 
     for ((i = 1); (i < n); i++)
     {
@@ -179,35 +83,25 @@ int main()
         if (abs(j) <= compare)
         {
             compare = abs(j);
-            left_board = i + 1;
+            num = i + 1;
         }
 
     }
-    std::cout << std::endl << left_board << std::endl;
+    return num;
+}
 
-    //task 20.2
-    left_board = arr[0];
+void Max(int* arr, int n) {
+    int max = arr[0], i, num;
     for (i = 1; (i <= n); i++)
-        if (left_board < arr[i])
+        if (max < arr[i])
         {
-            left_board = arr[i];
-            j = i;
+            max = arr[i];
+            num = i;
         }
-    std::cout << left_board << " " << (j + 1) << std::endl;
+    std::cout << max << " " << (num + 1) << std::endl;
+}
 
-
-    //extra
-
-    j = 0;
-    std::cout << "enter boards of  section";
-    std::cin >> left_board >> right_board;
-    if (left_board > right_board)
-    {
-        change = left_board;
-        left_board = right_board;
-        right_board = change;
-    }
-
+void DeleteElements(int* arr, int n, int left_board, int right_board) {
     size_t k = 0;
 
     for (size_t i = 0; i < n; i++)
@@ -222,12 +116,5 @@ int main()
     for (size_t i = k; i < n; i++) arr[i] = 0;
 
     for (size_t i = 0; i < n; i++) std::cout << arr[i] << ' ';
-
-
-
-
-
-
-    return 0;
->>>>>>> bb22c8262c06c2e83e431456e151447bba396fae
 }
+
--- semester_1/lab3_pointers_and_arrays/lab3(38).cpp
+++ semester_1/lab3_pointers_and_arrays/lab3(38).cpp
@@ -1,6 +1,10 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <cmath>
+int Sum(int* arr, int n);
+void TransformArray(int* arr, int n);
+
+
+int MaxEl(int* arr, int n);
 
 int main()
 {
@@ -52,6 +56,25 @@ int main()
 
     //task 38.2
 
+    std::cout << Sum(arr, n);
+
+    std::cout << std::endl;
+    //extra
+    TransformArray(arr, n);
+
+    std::cout << std::endl;
+    //task 38.1
+
+
+    std::cout << "the biggest element which occurs less often is " << MaxEl(arr, n);
+
+    delete[] arr;
+    arr = nullptr;
+    return 0;
+}
+
+int Sum(int* arr, int n) {
+    int i, j = 0, a = 0, sum = 0;
     for (i = 1; (i <= n); i++)
         if (arr[i] == 0)
         {
@@ -64,21 +87,25 @@ int main()
             a = i;
             break;
         }
-    if (j == a)  
+    if (j == a) {
         std::cout << "This action cannot be performed on this array" << std::endl;
+        return 0;
+    }
     else
     {
         for ((i = j); (i < a); i++)
         {
             sum += arr[i];
         }
-        std::cout << std::endl << "sum of elements between 0s is " << sum;
+        std::cout << std::endl << "sum of elements between 0s is ";
     }
     std::cout << std::endl;
+    return sum;
+}
 
 
-
-    //extra
+void TransformArray(int* arr, int n) {
+    int i, min = 0, j;
     for (i = 0; i < n; i++)
     {
         for (j = i + 1; j < n; j++)
@@ -89,18 +116,17 @@ int main()
                 arr[j] = min;
             }
 
-    } 
+    }
     std::cout << "transformed array: ";
     for (i = 0; i < n; i++)
     {
         std::cout << arr[i] << " ";
     }
-    std::cout << std::endl;
+}
 
 
-
-    //task 38.1
-   
+int MaxEl(int* arr, int n) {
+    int i, j, AbsNum, min;
     for (i = 0; i < n; i++)
     {
         for (j = i + 1; j < n; j++)
@@ -116,248 +142,36 @@ int main()
 
 
     for (i = 0; i < n; i++)
-	{
-		AbsNum = arr[i]+abs(arr[n-1]);
-		counter[AbsNum]++;
-	}
+    {
+        AbsNum = arr[i] + abs(arr[n - 1]);
+        counter[AbsNum]++;
+    }
 
 
-	min = 1000;
-	for (i = 0; i < n; i++)
-	{
-		AbsNum = arr[i] + abs(arr[n - 1]);
-	        if((min>counter[AbsNum])&&(counter[AbsNum]!=0))
-                 min=counter[AbsNum];
-	    if (min == 1) break;
-	}
+    min = 1000;
+    for (i = 0; i < n; i++)
+    {
+        AbsNum = arr[i] + abs(arr[n - 1]);
+        if ((min > counter[AbsNum]) && (counter[AbsNum] != 0))
+            min = counter[AbsNum];
+        if (min == 1) break;
+    }
 
-	std::cout<< std::endl;
+    std::cout << std::endl;
 
 
     for (i = 0; i < n; i++)
     {
         AbsNum = arr[i] + abs(arr[n - 1]);
-       
+
         if (counter[AbsNum] == min)
         {
 
-            std::cout <<"the biggest element which occurs less often is "<< arr[i];
+            return arr[i];
+            delete[] counter;
+            counter = nullptr;
             break;
         }
     }
 
-
-    delete[] arr;
-    arr = nullptr;
-    delete[] counter;
-    counter = nullptr;
-    return 0;
-
-=======
-#include <iostream>
-#include <cmath>
-
-int main()
-{
-    int* arr = new int[1000];
-    int sumaft = 0, sumbef = 0, j = 0, i, upper_board, lower_board, n, change, a = 0, min, sum = 0, abss = 0;
-    int how;
-
-    std::cout << "Enter the amount of elemens: ";
-    std::cin >> n;
-
-    if ((n <= 0) || (n > 1000)) return 0;
-    std::cout << "enter 0, if the elements of array are entered randomly" << std::endl << "Enter 1 if the elements of array are entered by hand ";
-
-    std::cin >> how;
-    if (how == 1)
-    {
-        for ((i = 0); (i < n); i++)
-        {
-            std::cout << "enter the element number" << (i + 1) << " ";
-            std::cin >> arr[i];
-        }
-    }
-    else if (how == 0)
-    {
-        std::cout << "Enter the lower board of array: ";
-        std::cin >> lower_board;
-        std::cout << "Enter the upper board of array: ";
-        std::cin >> upper_board;
-        if (lower_board > upper_board)
-        {
-            change = lower_board;
-            lower_board = upper_board;
-            upper_board = change;
-        }
-        for ((i = 0); (i < n); i++)
-        {
-            arr[i] = lower_board + rand() % (upper_board - lower_board);
-            std::cout << arr[i] << " ";
-        }
-    }
-    else
-    {
-        std::cout << "ONLY 1 OR 0!!!";
-        return 0;
-    }
-
-    std::cout << std::endl;
-
-
-    //task 38.2
-
-
-
-    for (i = 1; (i <= n); i++)
-        if (arr[i] == 0)
-        {
-            j = i;
-            break;
-        }
-    for (i = n; (i >= 0); i--)
-        if (arr[i] == 0)
-        {
-            a = i;
-            break;
-        }
-    if (j == a) return 0;
-    else
-    {
-        for ((i = j); (i < a); i++)
-        {
-            sum += arr[i];
-        }
-    }
-    std::cout << std::endl << sum << std::endl;
-
-
-    //extra
-    for (i = 0; i < n; i++)
-    {
-        for (j = i + 1; j < n; j++)
-            if ((arr[i] < 0) && (arr[j] >= 0))
-            {
-                min = arr[i];
-                arr[i] = arr[j];
-                arr[j] = min;
-            }
-
-    } for (i = 0; i < n; i++)  std::cout << arr[i] << " ";
-
-
-    //task 38.1
-    min = arr[0];
-    for (i = 0; i < n; i++)
-    {
-        for (j = i + 1; j < n; j++)
-            if (arr[i] > arr[j])
-            {
-                min = arr[i];
-                arr[i] = arr[j];
-                arr[j] = min;
-            }
-    }
-    for (i = 0; i < n; i++)
-        std::cout << arr[i] << " ";
-
-    std::cout << std::endl;
-
-    int* counter = new int[10000];
-    for (i = 0; i <= arr[n - 1]; i++)
-    {
-        abss = arr[i] + abs(arr[0]);
-        counter[abss] = 0;
-    }
-
-
-    for (i = 0; i < n; i++)
-    {
-        abss = arr[i] + abs(arr[0]);
-        counter[abss]++;
-    }
-
-
-
-    for (i = arr[0]; i <= arr[n - 1]; i++)
-    {
-        abss = arr[i] + abs(arr[0]);
-        if ((counter[abss] < min) && (counter[abss] > 0))
-            min = counter[abss];
-    }
-
-
-    for (i = n - 1; i >= 0; i--)
-    {
-        if (counter[abss] == min)
-        {
-            std::cout << "the biggest element is " << arr[i];
-            break;
-        }
-    }
-
-
-
-    //task 38.1
-    min = arr[0];
-    for (i = 0; i < n; i++)
-    {
-        for (j = i + 1; j < n; j++)
-            if (arr[i] > arr[j])
-            {
-                min = arr[i];
-                arr[i] = arr[j];
-                arr[j] = min;
-            }
-    }
-    for (i = 0; i < n; i++)
-        std::cout << arr[i] << " ";
-
-    std::cout << std::endl;
-
-    int* counter = new int[10000];
-    for (i = 0; i <= arr[n - 1]; i++)
-    {
-        abss = arr[i] + abs(arr[0]);
-        counter[abss] = 0;
-    }
-
-
-    for (i = 0; i < n; i++)
-    {
-        abss = arr[i] + abs(arr[0]);
-        counter[abss]++;
-    }
-
-
-
-    for (i = arr[0]; i <= arr[n - 1]; i++)
-    {
-        abss = arr[i] + abs(arr[0]);
-        if ((counter[abss] < min) && (counter[abss] > 0))
-            min = counter[abss];
-    }
-
-
-    for (i = n - 1; i >= 0; i--)
-    {
-        if (counter[abss] == min)
-        {
-            std::cout << "the biggest element is " << arr[i];
-            break;
-        }
-    }
-
-
-
-
-
-
-    delete[] arr;
-    arr = nullptr;
-    delete[] counter;
-    counter = nullptr;
-    return 0;
-
->>>>>>> bb22c8262c06c2e83e431456e151447bba396fae
 }
