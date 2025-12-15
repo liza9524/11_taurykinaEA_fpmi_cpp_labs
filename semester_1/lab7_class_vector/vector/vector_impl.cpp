c2fa1b873dac3076c62daf6a51e43e32b89da8a7
rachel.i.jonathan.pobedili@gmail.com <rachel.i.jonathan.pobedili@gmail.com>
2025-12-15 23:18:04 +0300

    lab 7 changed

--- semester_1/lab7_class_vector/vector/vector_impl.cpp
+++ semester_1/lab7_class_vector/vector/vector_impl.cpp
@@ -1,43 +1,47 @@
 #include "vector_impl.h"
 
-Vector::Vector() {}
-
+Vector::Vector() : size_(0), capacity_(0), arr_(nullptr) {
+}
 Vector::Vector(size_t size) : size_(size), arr_(new int[size_]), capacity_(size_) 
 {
-    for (int i = 0; i < size_; i++) {
+    for (size_t i = 0; i < size_; i++) {
         arr_[i] = 0;
     }
 }
 
-Vector::Vector(std::initializer_list<char> list)
-    : size_(list.size()), arr_(new int[size_]), capacity_(size_) {
+Vector::Vector(std::initializer_list<int> list)
+    : size_(list.size()), capacity_(list.size()), arr_(new int[capacity_]) {
     size_t i = 0;
-    for (size_t item : list) {
+    for (int item : list) {
         arr_[i] = item;
         ++i;
     }
 }
 
-Vector::Vector(size_t size, int a) : size_(size), arr_(new int[size_]), capacity_(size_) {
-    for (int i = 0; i < size_; i++) {
-        arr_[i] = a;
+Vector::Vector(const Vector& other)
+    : size_(other.size_),
+      capacity_(other.capacity_),
+      arr_(other.capacity_ > 0 ? new int[capacity_] : nullptr) {
+    for (size_t i = 0; i < size_; ++i) {
+        arr_[i] = other.arr_[i];
     }
 }
 
 Vector& Vector::operator=(const Vector& other) {
-    if (this == &other)
-        return *this;
-    delete[] arr_;
-    size_ = other.size_;
-    arr_ = new int[size_];
-    for (size_t i = 0; i < size_; ++i) {
-        arr_[i] = other.arr_[i];
+    if (this != &other) {
+        delete[] arr_;
+        size_ = other.size_;
+        capacity_ = other.capacity_; 
+        arr_ = new int[capacity_];
+        for (size_t i = 0; i < size_; ++i) {
+            arr_[i] = other.arr_[i];
+        }
     }
     return *this;
 }
 
 Vector::~Vector() {
-    delete[] arr_;
+    Clear();
 }
 void Vector::Swap(Vector& v1) {
     std::swap(size_, v1.size_);
@@ -52,24 +56,23 @@ int& Vector::operator[](size_t index) {
 const int& Vector::operator[](size_t index) const {
     return arr_[index];
 }
-
-int& Vector::at(int n) {
+int& Vector::At(size_t n) {
     if (n >= size_)
-        throw std::out_of_range("wrong index");
+        throw std::out_of_range("Wrong index");
     return arr_[n];
 }
 
-const int& Vector::at(int n) const {
+const int& Vector::At(size_t n) const {
     if (n >= size_)
-        throw std::out_of_range("wrong index");
+        throw std::out_of_range("Wrong index");
     return arr_[n];
 }
 
-size_t Vector::Size() {
+size_t Vector::Size() const{
     return size_;
 }
 
-size_t Vector::Capacity() {
+size_t Vector::Capacity() const{
     return capacity_;
 }
 
@@ -79,36 +82,38 @@ void Vector::PopBack() {
     }
 }
 
-void Vector::Clean() {
-    size_ = 0;
+void Vector::Clear() 
+{
+        delete[] arr_;
+        arr_ = nullptr;
+        size_ = 0;
+        capacity_ = 0;
 }
 
-void Vector::PushBack(int n) {
-    if (size_ == Capacity()) {
-        size_t new_size_ = 2 * size_;
-        int* new_arr_ = new int[new_size_];
-        for (int i = 0; i < size_; ++i) {
+
+void Vector::PushBack(int n) 
+{
+    if (size_ == capacity_) 
+    {
+        size_t new_capacity = (capacity_ == 0) ? 1 : 2 * capacity_;
+        Reserve(new_capacity);
+    }
+    arr_[size_] = n; 
+    ++size_;       
+
+}
+void Vector::Reserve(size_t new_capacity) {
+    if (new_capacity > capacity_) {
+        int* new_arr_ = new int[new_capacity];
+        for (size_t i = 0; i < size_; ++i) {
             new_arr_[i] = arr_[i];
         }
         delete[] arr_;
         arr_ = new_arr_;
-        capacity_ *= 2;
+        capacity_ = new_capacity;
     }
+}
 
-    ++size_;
-    arr_[size_ - 1] = n;
-}
-void Vector::Reserve(size_t RCapacity) {
-    if (RCapacity > Capacity()) {
-        int* new_arr_ = new int[RCapacity];
-        for (int i = 0; i < size_; ++i) {
-            new_arr_[i] = arr_[i];
-        }
-        delete[] arr_;
-        arr_ = new_arr_;
-        capacity_ = RCapacity;
-    }
-}
 
 std::ostream& operator<<(std::ostream& out, const Vector& v) {
     out << std::endl << "[";
--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -2,26 +2,26 @@
 #include <ostream>
 class Vector {
 private:
-    size_t size_ = 0;
-    size_t capacity_ = 0;
-    int* arr_ = nullptr;
+    size_t size_;
+    size_t capacity_;
+    int* arr_;
 
 public:
     Vector();
     Vector(size_t size);
-    Vector(std::initializer_list<char> list);
-    Vector(size_t size, int a);
+    Vector(std::initializer_list<int> list);
+    Vector(const Vector& other);
     Vector& operator=(const Vector& other);
     ~Vector();
     void Swap(Vector& v1);
     int& operator[](size_t index);
     const int& operator[](size_t index) const;
-    int& at(int n);
-    const int& at(int n) const;
-    size_t Size();
-    size_t Capacity();
+    int& At(size_t n);
+    const int& At(size_t n) const;
+    size_t Size() const;
+    size_t Capacity() const;
     void PopBack();
-    void Clean();
+    void Clear();
     void PushBack(int n);
     void Reserve(size_t RCapacity);
     friend std::ostream& operator<<(std::ostream& out, const Vector& v);
